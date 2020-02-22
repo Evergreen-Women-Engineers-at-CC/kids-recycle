142c655068e26ac70ee3be437cc573dc04605fbf
DagnyDare <anddag30@evergreen.edu>
2020-02-22 23:24:17 +0000

    added file path to database, error image file

--- kidsRecycle/sortingCans/models.py
+++ kidsRecycle/sortingCans/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 class Trash(models.Model):
     name = models.CharField(max_length=200)
+    filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
 class Recycle(models.Model):
     name = models.CharField(max_length=200)
+    filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
 class Compost(models.Model):
     name = models.CharField(max_length=200)
+    filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
 
