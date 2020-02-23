62f6ee4a7b045ec6a9dbaa2836abef30b636e605
LuceraCox <cox.lucera@gmail.com>
2020-02-22 23:41:35 -0800

    added dunder self methods to bin models

--- kidsRecycle/sortingCans/models.py
+++ kidsRecycle/sortingCans/models.py
@@ -6,16 +6,25 @@ class Trash(models.Model):
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class Recycle(models.Model):
     name = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class Compost(models.Model):
     name = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
