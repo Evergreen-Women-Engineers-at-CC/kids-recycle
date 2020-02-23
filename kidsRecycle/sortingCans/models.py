0c0ba3edd8c161da7ddb085b9561f8424c7d6cd9
LuceraCox <cox.lucera@gmail.com>
2020-02-22 22:25:52 -0800

    registered models in Django Admin

--- kidsRecycle/sortingCans/models.py
+++ kidsRecycle/sortingCans/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
+
 class Trash(models.Model):
     name = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
+
 
 class Recycle(models.Model):
     name = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
 
+
 class Compost(models.Model):
     name = models.CharField(max_length=200)
     filepath = models.CharField(max_length=200, default='kids-recycle/kidsRecycle/sortingCans/media/error.png')
     points = models.IntegerField(default=0)
-
 
 # Create your models here.
