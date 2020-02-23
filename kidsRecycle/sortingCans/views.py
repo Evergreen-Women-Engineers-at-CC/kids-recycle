6cff720ca8c3848f77f60345e74fe7fb9e69a36c
Selena <selenawalshsmith@gmail.com>
2020-02-23 02:39:13 -0800

    finally added something to template from database

--- kidsRecycle/sortingCans/views.py
+++ kidsRecycle/sortingCans/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.db.models.aggregates import Count
-from random import randint
 from .models import Recycle
 from .models import Trash
 from .models import Compost
 
-
+def getpic(self):
+    return
 def index(request):
     all_recycle = Recycle.objects.all()
     all_trash = Trash.objects.all()
     compost = Compost.objects.all()
-    thing = Compost.objects.get(id=2)
 
-    #number_of_compost = Compost.objects.count()
-    #random_index = int(random.random()*number_of_compost)+1
-#    random_compost = Compost.get(pk= random_index)
-    random_compost = Compost.objects.order_by('?').first()
+    #this is what we were able to render in the view
+    thing = Compost.objects.get(id=2)
+    compost_pic = thing.filepath
+
 
     template = loader.get_template('sortingCans/index.html')
     context = {
         'all_recycle': all_recycle,
         'all_trash' : all_trash,
         'compost' : compost,
-        'random_compost' : random_compost,
-        'thing' : thing
+        'thing' : thing,
+        #dictionary definition sending "context" to the template
+        'compost_pic' : thing
     }
     return HttpResponse(template.render(context,request))
 
